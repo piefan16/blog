53f6f70b92216e854a3775fc22fd1d579f88ee2e
piefan16 <piefan16@gmail.com>
2014-07-28 03:31:14 +0200

    Test 2 for the change

--- app/config/local/database.php
+++ app/config/local/database.php
@@ -25,7 +25,7 @@ return array(
 			'host'      => 'localhost',
 			'database'  => 'blog',
 			'username'  => 'root',
-			'password'  => '',
+			'password'  => 'change',
 			'charset'   => 'utf8',
 			'collation' => 'utf8_unicode_ci',
 			'prefix'    => '',
